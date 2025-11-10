ad20d8742576e1d96f8fefd97bce92ab756eafcf
sarish339 <sarishsunkara339@gmail.com>
2025-11-10 10:19:25 +0530

    shell

---
+++ frontend.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo  -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then
+    echo -e "$2 is $R failed $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G success $N" | tee -a $LOG_FILE
+    fi  
+}
+
+echo "Script started executing:: $(date)"  | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "Enable nginx"
+
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "Start nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing defaulu website"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Extarct frontend code"
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarted frontend"
+
